6cdc7a059f11429dddee7fc1aa883d3dfe74884d
alexanderMontague <xturnermonty@gmail.com>
2019-03-21 19:37:55 -0400

    hot reloading, and finalize salary route

--- app/src/controllers/salary.js
+++ app/src/controllers/salary.js
@@ -1,25 +1,31 @@
-const ontario2017 = require('../salaryData/ontario2017.json');
+const ontario2010 = require('../data/ontario2010.json');
+const ontario2011 = require('../data/ontario2011.json');
+const ontario2012 = require('../data/ontario2012.json');
 
 /*
  *   GET /api/salaryData
  *
  *   REQ: {
- *
+ *      body || query: {
+ *        year: Number
+ *      }
  *   }
  *
  *   RES: {
  *     response: {
- *       code: Integer,
- *       message: String,
- *       data: Object || Array || null,
- *       error: Boolean
+ *       salaryData: Array || null
  *     }
  *   }
  */
 function fullSalaryData(req, res) {
   const salaryYear = req.query.year || req.body.year;
+  const salaryFile = eval('ontario' + salaryYear);
 
-  return res.json(eval('ontario' + salaryYear));
+  if (!salaryFile) {
+    return res.json({ salaryData: null });
+  }
+
+  return res.json({ salaryData: salaryFile });
 }
 
 module.exports = {
