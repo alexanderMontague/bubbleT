f549f9c0a4b0eb4514c0d66b1f503a4432d40d14
alexanderMontague <xturnermonty@gmail.com>
2019-03-21 19:15:05 -0400

    restructure pt2

--- server/src/routesPublic.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const router = require('express').Router();
-const { home, salary } = require('./controllers');
-
-router.route('/test').get(home.test);
-router.route('/salaryData').get(salary.fullSalaryData);
-
-module.exports = router;
