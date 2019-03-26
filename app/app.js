f8c2f3bb20c3a3b139ec819936b9c23fcc2238f0
amahmoud33 <ahmedesmahmoud@gmail.com>
2019-03-26 14:59:34 -0400

    - Removed legacy template folder - Added previous functionality to salaries.js - app.js now serves new public folder

--- app/app.js
+++ app/app.js
@@ -68,7 +68,7 @@ app.use(expressValidator());
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(flash());
-app.use(express.static('./template'));
+app.use(express.static('./public'));
 app.use(cors(corsOptions)); // todo set CORS up
 app.disable('x-powered-by');
 
